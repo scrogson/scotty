fe393478a462127ceb894891477ae1384ab1d183
Sonny Scroggin <scrogson@gmail.com>
2015-10-12 22:05:50 -0500

    Remove OP_PRINT and just print the return in main() for now

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 // Bytecode instructions
 const OP_LOAD: u8 = 1; // arity: 2 - (value: u64, dest: u8)
 const OP_ADD: u8 = 2; // arity: 3 - (a: u8, b: u8, dest: u8)
-const OP_PRINT: u8 = 3; // arity: 1 - (reg: u8)
-const OP_RETURN: u8 = 4; // arity: 1 - (reg: u8)
+const OP_RETURN: u8 = 3; // arity: 1 - (reg: u8)
 
 fn interpret(code: &[u8]) -> u64 {
     let mut pc = 0; // "Program counter", the index of the current instruction.
@@ -30,14 +29,8 @@ fn interpret(code: &[u8]) -> u64 {
                 pc += 1;
                 regs[dest] = regs[a] + regs[b];
             },
-            OP_PRINT => {
-                let r = code[pc] as usize;
-                pc += 1;
-                println!("{}", regs[r]);
-            },
             OP_RETURN => {
                 let r = code[pc] as usize;
-                pc += 1;
                 return regs[r];
             },
             _ => panic!("Invalid opcode at offset {}", pc)
@@ -50,9 +43,9 @@ fn main() {
         OP_LOAD, 73, 0, 0, 0, 0, 0, 0, 0, 0, // load the number 73 into R0.
         OP_LOAD, 68, 0, 0, 0, 0, 0, 0, 0, 1, // load the number 68 into R1.
         OP_ADD, 0, 1, 2, // compute R0 + R1 and store it in R2.
-        OP_PRINT, 2,
         OP_RETURN, 2
     ];
 
-    interpret(&code); // should print 73 + 68, which is 141.
+    // should print 73 + 68, which is 141.
+    println!("{}", interpret(&code))
 }
