9d2ca75af76e976b2a3c0bfbb86595e15a0a66a9
Sonny Scroggin <scrogson@gmail.com>
2015-10-15 22:34:08 -0500

    Add operation

--- src/emitter.rs
+++ src/emitter.rs
@@ -1,10 +1,11 @@
 use interpreter::*;
 
-enum Program {
-    IntLiteral(u8)
+pub enum Program {
+    IntLiteral(u8),
+    Binary {op: u8, left: u8, right: u8}
 }
 
-fn emit(program: Program) -> Vec<u8> {
+pub fn emit(program: Program) -> Vec<u8> {
     let mut code = vec![];
     match program {
         Program::IntLiteral(n) => {
@@ -13,6 +14,20 @@ fn emit(program: Program) -> Vec<u8> {
             code.push(0);
             code.push(OP_RETURN);
             code.push(0);
+        },
+        Program::Binary {op, left, right} => {
+            code.push(OP_LOAD_U8);
+            code.push(left);
+            code.push(0);
+            code.push(OP_LOAD_U8);
+            code.push(right);
+            code.push(1);
+            code.push(op);
+            code.push(0);
+            code.push(1);
+            code.push(2);
+            code.push(OP_RETURN);
+            code.push(2);
         }
     }
     code
@@ -22,4 +37,14 @@ fn emit(program: Program) -> Vec<u8> {
 fn test_emit() {
     let code1 = emit(Program::IntLiteral(37));
     assert_eq!(37, interpret(&code1));
+
+
+    let program = Program::Binary {
+        op: OP_ADD,
+        left: 73,
+        right: 68
+    };
+
+    let code2 = emit(program);
+    assert_eq!(141, interpret(&code2));
 }
